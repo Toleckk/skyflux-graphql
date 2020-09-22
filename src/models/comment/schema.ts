d213e95272bdbff26655646aa78b16b9ce3d4098
Toleckk <anatolich1995@gmail.com>
2020-09-22 12:35:42 +0300

    Add Comment Connection schema

--- src/models/comment/schema.ts
+++ src/models/comment/schema.ts
@@ -6,7 +6,17 @@ export const CommentSchema = `
         user: User!
         post: Post!
     }
-    
+
+    type CommentEdge implements Edge {
+        cursor: ID!
+        node: Comment!
+    }
+
+    type CommentConnection implements Connection {
+        edges: [CommentEdge]!
+        pageInfo: PageInfo!
+    }
+
     extend type Mutation {
         createComment(post_id: ID!, text: String!): Comment!
         deleteComment(_id: ID!): Boolean
