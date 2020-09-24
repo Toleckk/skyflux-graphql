85f0f12b5f1585fbf561b75c49b24b4dedeec8c8
Toleckk <anatolich1995@gmail.com>
2020-09-24 21:34:05 +0300

    Add resolveSub function

--- src/models/sub/service.ts
+++ src/models/sub/service.ts
@@ -1,4 +1,6 @@
+import Mongoose from 'mongoose'
 import {User, UserService} from '@models/user'
+import {isMongoId} from '@utils/isMongoId'
 import {Sub} from './types'
 import {SubModel} from './model'
 
@@ -35,4 +37,25 @@ export const removeSub = async ({
 
   const deleted = await SubModel.deleteOne({from_id: user._id, to_id: to._id})
   return (deleted?.deletedCount || 0) > 0
+}
+
+export const getSubById = async ({
+  _id,
+}: {
+  _id: string | Mongoose.Types.ObjectId
+}): Promise<Partial<Sub> | null> => SubModel.findById(_id)
+
+export const resolveSub = ({
+  root,
+}: {
+  root:
+    | {sub: Partial<Sub>}
+    | {sub_id: Partial<Sub> | string | Mongoose.Types.ObjectId}
+}): Promise<Partial<Sub> | null> | Partial<Sub> => {
+  if ('sub' in root) return root.sub
+
+  if (typeof root.sub_id !== 'string' && !isMongoId(root.sub_id))
+    return root.sub_id
+
+  return getSubById({_id: root.sub_id})
 }
