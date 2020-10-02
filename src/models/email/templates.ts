aa0f0564792cb23aa765312fa55b73f1dd77c0bd
Toleckk <anatolich1995@gmail.com>
2020-10-02 09:33:45 +0300

    Add email templates

---
+++ src/models/email/templates.ts
@@ -0,0 +1,34 @@
+import {User} from '@models/user'
+
+export const restore = ({user, token}: {user: User; token: string}): string => `
+  <h1 align='center'>
+    Skyflux
+  </h1>
+  <br/>
+  <h4 align='center'>
+    Hello, ${user.nickname}!
+  </h4>
+  <br/> 
+  You've started password resetting procedure. Follow the link to change your password: 
+  <br/>
+  <a href="${process.env.FRONT_URL}/id/reset/${token}">${process.env.FRONT_URL}/id/reset/${token}</a>
+  <br/>
+  <span align="center">If you haven't started this procedure, just ignore this email</span>
+  </br>  
+`
+
+export const confirm = ({user, token}: {user: User; token: string}): string => `
+<div>
+  <h1 align='center'>
+    Skyflux
+  </h1>
+  <br />
+  <h4 align='center'>
+    Welcome, ${user.nickname}! Follow the link to confirm your email:
+    <br />
+    <a href="${process.env.FRONT_URL}/user/confirm-email/${token}">${process.env.FRONT_URL}/user/confirm-email/${token}</a>
+  </h4>
+  <span align="center">If you didn't request this email, just ignore it</span>
+  </br>  
+</div>
+`
