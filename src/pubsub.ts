d1f903abbb52c3618dbe41eb3e3961d1b9e57473
Toleckk <anatolich1995@gmail.com>
2020-11-09 20:34:16 +0300

    Configure redis pubsub

--- src/pubsub.ts
+++ src/pubsub.ts
@@ -1,3 +1,15 @@
 import {PubSub} from 'apollo-server'
+import {RedisPubSub} from 'graphql-redis-subscriptions'
+import Redis from 'ioredis'
 
-export const pubsub = new PubSub()
+const options = {
+  retryStrategy: (times: number): number => Math.min(times * 50, 2000),
+}
+
+export const pubsub =
+  process.env.NODE_ENV === 'production'
+    ? new RedisPubSub({
+        publisher: new Redis(process.env.REDIS_URL, options),
+        subscriber: new Redis(process.env.REDIS_URL, options),
+      })
+    : new PubSub()
